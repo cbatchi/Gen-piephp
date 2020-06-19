04842acade3f13272c47db56430404498d217722
Claude BATCHI <56683920+cbatchi@users.noreply.github.com>
2020-06-19 19:04:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-// Remove executable file to array
+
+// Remove executable file index to array
 function get_args () {
   global $argv;
   array_shift($argv);
@@ -13,7 +14,7 @@ function createDirectory (array $tabs, $dir=false) {
     foreach ($tabs as $key => $value) {
       if (isset($key)) {
         shell_exec("mkdir $dir/$key");
-        if ($key == 'Core') {
+        if ($key == 'Core') { 
           foreach($value as $content) {
             $upper = ucfirst($content);
             shell_exec("cd $key && touch $upper.php && echo '<?php\r\rnamespace\t$key;\r\rclass\t$upper\t{\r\r}\r' > $upper.php");
@@ -21,7 +22,7 @@ function createDirectory (array $tabs, $dir=false) {
         } elseif ($key == 'src') {
           foreach($value as $content) {
             $upper = ucfirst($content);
-            shell_exec("cd $key && mkdir $upper");
+            shell_exec("cd $key && mkdir $upper && touch ./routes.php && echo '<?php\r\r' > ./routes.php");
           }
         } else {
           foreach($value as $content) {
@@ -33,32 +34,51 @@ function createDirectory (array $tabs, $dir=false) {
   }
 }
 
-// Run action
+// Run function
 function run () {
 
+  // Directory names
   $paths = [
-    "Core" => ["core","controller","database","entity","ORM","request","router"],
+    "Core" => ["core","controller","database","entity","ORM","request","router","templateEngine"],
     "src" => ['controller','model','view'],
     'public' => ['css','js','assets']
   ];
 
+  // Current dir path
   $currentDir = __DIR__;
   $arg = get_args(); 
   
+  // Actions list, just create work for the moment 
   $actions = ['create','update','delete'];
+  
 
-  if ($arg[0] == $actions[0]) { createDirectory($paths ,$currentDir); }
-  else { return false; exit(); }
+  // Statement if first index match with create word
+  $arg[0] == $actions[0] ? createDirectory($paths ,$currentDir) : false;
+  
   // Ask for empty content index.php
-  echo "Les dossiers ont été créer" . "\n\n";
-  echo "T'es à présent prêt à bosser\n\n";
+  echo '| ================================= |' . "\n\n\r";
+    echo "  Le skeleton mvc a bien été créer" . "\n\n";
+  echo '| ================================= |' . "\n\n\r";
 
-  $ask = (string) readline('Un dernier details! veux tu reset ce fichier index.php ? [O/N]');
-  if ($ask === "O") {
-    shell_exec("echo '' > ./index.php && echo '<?php\r\r' > ./index.php && rm -rf readme.md");
-  } elseif($ask === "N") {
-    echo 'Operation abandonné :)' . PHP_EOL;
-    return false;
+  // Ask question for delete content of index file for start coding
+  $ask = (string) readline('Un dernier detail! souhaites tu vider le contenu du fichier index ? [O/N] ');
+
+  // Switch case statement if enter expected word make action
+  switch ($ask) {
+    
+    case 'O':
+      shell_exec("echo '' > ./index.php && echo '<?php\r\r'// Happy coding !!! > ./index.php && rm -rf README.md");
+      return true;
+    break;
+    
+    case 'N':
+      echo 'A bientôt !' . PHP_EOL;
+      return false;
+    break;
+    
+    default:
+      echo 'Aucune de ses options n\'a été reconnue';
+      return false;
   }
 }
 
